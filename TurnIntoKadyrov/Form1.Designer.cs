22ee16d2c0f5e188a142facd0b8960e904fe284e
MrYotic <yoticcitoy@gmail.com>
2022-04-06 08:29:07 +0300

    Добавьте файлы проекта.

---
+++ TurnIntoKadyrov/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TurnIntoKadyrov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseB = new System.Windows.Forms.Button();
+            this.ДонДонДон = new System.Windows.Forms.CheckBox();
+            this.Z = new System.Windows.Forms.CheckBox();
+            this.AmplitudeT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.V = new System.Windows.Forms.CheckBox();
+            this.AddInfo = new System.Windows.Forms.CheckBox();
+            this.Enable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CloseB
+            // 
+            this.CloseB.FlatAppearance.BorderSize = 0;
+            this.CloseB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseB.ForeColor = System.Drawing.Color.Silver;
+            this.CloseB.Location = new System.Drawing.Point(502, 2);
+            this.CloseB.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.CloseB.Name = "CloseB";
+            this.CloseB.Size = new System.Drawing.Size(26, 25);
+            this.CloseB.TabIndex = 0;
+            this.CloseB.Text = "X";
+            this.CloseB.UseVisualStyleBackColor = true;
+            this.CloseB.Click += new System.EventHandler(this.CloseB_Click);
+            // 
+            // ДонДонДон
+            // 
+            this.ДонДонДон.AutoSize = true;
+            this.ДонДонДон.ForeColor = System.Drawing.Color.Silver;
+            this.ДонДонДон.Location = new System.Drawing.Point(13, 441);
+            this.ДонДонДон.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ДонДонДон.Name = "ДонДонДон";
+            this.ДонДонДон.Size = new System.Drawing.Size(1076, 19);
+            this.ДонДонДон.TabIndex = 1;
+            this.ДонДонДон.Text = "Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Д" +
+    "он Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Дон Д" +
+    "он Дон ";
+            this.ДонДонДон.UseVisualStyleBackColor = true;
+            // 
+            // Z
+            // 
+            this.Z.AutoSize = true;
+            this.Z.ForeColor = System.Drawing.Color.Silver;
+            this.Z.Location = new System.Drawing.Point(13, 416);
+            this.Z.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Z.Name = "Z";
+            this.Z.Size = new System.Drawing.Size(32, 19);
+            this.Z.TabIndex = 2;
+            this.Z.Text = "Z";
+            this.Z.UseVisualStyleBackColor = true;
+            // 
+            // AmplitudeT
+            // 
+            this.AmplitudeT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.AmplitudeT.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AmplitudeT.ForeColor = System.Drawing.Color.Silver;
+            this.AmplitudeT.Location = new System.Drawing.Point(64, 464);
+            this.AmplitudeT.Name = "AmplitudeT";
+            this.AmplitudeT.Size = new System.Drawing.Size(100, 15);
+            this.AmplitudeT.TabIndex = 3;
+            this.AmplitudeT.Text = "5";
+            this.AmplitudeT.TextChanged += new System.EventHandler(this.AmplitudeT_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 463);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Частота:";
+            // 
+            // V
+            // 
+            this.V.AutoSize = true;
+            this.V.ForeColor = System.Drawing.Color.Silver;
+            this.V.Location = new System.Drawing.Point(53, 416);
+            this.V.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(33, 19);
+            this.V.TabIndex = 5;
+            this.V.Text = "V";
+            this.V.UseVisualStyleBackColor = true;
+            // 
+            // AddInfo
+            // 
+            this.AddInfo.AutoSize = true;
+            this.AddInfo.ForeColor = System.Drawing.Color.Silver;
+            this.AddInfo.Location = new System.Drawing.Point(94, 416);
+            this.AddInfo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.AddInfo.Name = "AddInfo";
+            this.AddInfo.Size = new System.Drawing.Size(164, 19);
+            this.AddInfo.TabIndex = 6;
+            this.AddInfo.Text = "Дополнительная правда";
+            this.AddInfo.UseVisualStyleBackColor = true;
+            // 
+            // Enable
+            // 
+            this.Enable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Enable.FlatAppearance.BorderSize = 0;
+            this.Enable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Enable.ForeColor = System.Drawing.Color.Silver;
+            this.Enable.Location = new System.Drawing.Point(275, 482);
+            this.Enable.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Enable.Name = "Enable";
+            this.Enable.Size = new System.Drawing.Size(240, 25);
+            this.Enable.TabIndex = 7;
+            this.Enable.Text = "Send rockets with hohlo-liquid to NATO";
+            this.Enable.UseVisualStyleBackColor = false;
+            this.Enable.Click += new System.EventHandler(this.Enable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(528, 519);
+            this.Controls.Add(this.Enable);
+            this.Controls.Add(this.AddInfo);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AmplitudeT);
+            this.Controls.Add(this.Z);
+            this.Controls.Add(this.ДонДонДон);
+            this.Controls.Add(this.CloseB);
+            this.Font = new System.Drawing.Font("Open Sans Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "The secret weapon Russia for dispose NATO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseB;
+        private System.Windows.Forms.CheckBox ДонДонДон;
+        private System.Windows.Forms.CheckBox Z;
+        private System.Windows.Forms.TextBox AmplitudeT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox V;
+        private System.Windows.Forms.CheckBox AddInfo;
+        private System.Windows.Forms.Button Enable;
+    }
+}
+
---
+++ TurnIntoKadyrov/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+namespace TurnIntoKadyrov
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+        private Thread updater = new Thread(() => new Form1().Update());
+        private Random rnd = new Random("За путина".Select(zа => (int)zа).Sum());
+        private static string lastText = "";
+        private int amplitude = 5;
+        private static bool enabled = false;
+        private string ConvertToKadyrov(string str) => string.Join(" ", Replace(str, new List<(dynamic, dynamic)>()
+            .Concat(Z.Checked ? new List<(dynamic, dynamic)> { ("з", "z"), ("З", "Z") } : new List<(dynamic, dynamic)>())
+            .Concat(V.Checked ? new List<(dynamic, dynamic)> { ("в", "v"), ("В", "V") } : new List<(dynamic, dynamic)>())
+            .Concat(AddInfo.Checked ? new List<(dynamic, dynamic)> { ("украинец", "хохол") } : new List<(dynamic, dynamic)>())
+            .ToArray()
+            ).Split(' ').ToList().Select(z => z + (rnd.Next(0, amplitude) == 0 ? " Дон" : "")));
+        private void Update()
+        {
+            while (true)
+                try
+                {
+                    Thread.Sleep(100);
+                    if (enabled && Clipboard.GetText() != lastText)
+                        Clipboard.SetText(lastText = ConvertToKadyrov(Clipboard.GetText()));
+                }catch { }
+        }
+        private void CloseB_Click(object sender, EventArgs e) => Environment.Exit(1);
+        private void AmplitudeT_TextChanged(object sender, EventArgs e) => int.TryParse(AmplitudeT.Text, out amplitude);
+        private void Enable_Click(object sender, EventArgs e) => Enable.Text = (enabled = !enabled) ? "Off" : "Send rockets with hohlo-liquid to NATO";
+        public string Replace(string str, params (dynamic, dynamic)[] args)
+        {
+            args.ToList().ForEach(z => str.Replace(z.Item1.ToSting(), z.Item2.ToString()));
+            return str;
+        }
+    }
+}
